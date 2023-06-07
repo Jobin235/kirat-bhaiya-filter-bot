f18e54528c8d5db7d410ae7953644915d1f0e8f0
Jobin Joy <jobinjoy235@gmail.com>
2023-06-07 19:20:44 +0530

    fix issue with word kirat getting filtered

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const stringSimilarity = require('string-similarity');
+const { Client, GatewayIntentBits } = require("discord.js");
+const stringSimilarity = require("string-similarity");
+
+require("dotenv").config();
 
 const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent
-    ]
-  });
-
-// Bot token - Replace 'YOUR_BOT_TOKEN' with your own bot token
-const BOT_TOKEN = 'YOUR_BOT_TOKEN';
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 // Specific words to trigger the DM
-const triggerWords = ['kirat bhaiya', 'kirat sir'];
+const triggerWords = ["kirat bhaiya", "kirat sir"];
 
 // Event: Ready
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Bot logged in as ${client.user.tag}`);
 });
 
 // Event: Message
-client.on('messageCreate', (message) => {
+client.on("messageCreate", (message) => {
   // Ignore messages from the bot itself
   if (message.author.bot) return;
 
   // Check if the message contains any trigger words
   const content = message.content.toLowerCase();
 
-    // Find the most similar trigger word
-    const matches = stringSimilarity.findBestMatch(content, triggerWords);
-    const bestMatch = matches.bestMatch;
+// Check if the message content is kirat
+  if (content === 'kirat') return;
 
-    // Check if the similarity ratio exceeds a threshold
+  // Find the most similar trigger word
+  const matches = stringSimilarity.findBestMatch(content, triggerWords);
+  const bestMatch = matches.bestMatch;
+
+  // Check if the similarity ratio exceeds a threshold
   if (bestMatch.rating >= 0.5) {
     const user = message.author;
     user.send(`${message.author} Call me Kirat`);
   }
-
 });
 
 // Login the bot to Discord
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
